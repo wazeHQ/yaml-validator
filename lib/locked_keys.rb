b9b0c5ca911cd305737e255b096d6a5f9f58bead
David Elentok <3david@gmail.com>
2013-10-02 12:26:39 +0300

    added missing locked_keys.rb

---
+++ lib/locked_keys.rb
@@ -0,0 +1,31 @@
+class LockedKeys
+  def initialize(root_path)
+    @locked_patterns = []
+
+    fullpath = File.join(root_path, 'locked_keys')
+    if File.exists? fullpath
+      read_locked_patterns(fullpath) unless File.directory?(fullpath)
+    end
+  end
+
+  def locked?(full_key)
+    @locked_patterns.each do |pattern|
+      if full_key =~ pattern
+        return true
+      end
+    end
+
+    false
+  end
+
+private
+
+  def read_locked_patterns(fullpath)
+    File.open(fullpath) do |f|
+      f.each_line do |line|
+        next if line.empty? or line.start_with? '#'
+        @locked_patterns << Regexp.new(line.rstrip)
+      end
+    end
+  end
+end
